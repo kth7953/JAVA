db60111f4906b3688d8ab7cf824248e2aa749ed9
kth7953 <101624909+kth7953@users.noreply.github.com>
2022-03-21 02:44:33 +0900

    Create String.java

---
+++ String.java
@@ -0,0 +1,15 @@
+package string;
+
+public class JavaSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 18;
+		String bin = Integer.toBinaryString(a);
+		System.out.println("int -> 문자열 이진수 : " + bin);
+
+	}
+
+}
+
+결과값: int -> 문자열 이진수 : 10010
